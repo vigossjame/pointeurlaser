7d0d1148ec2c5342c80fdfdad5802cdfa6ff52df
vigossjame <gabinvigoss@gmail.com>
2022-04-17 23:53:46 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,21 +1,16 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>W34534534534534534534534atic</h1>
+    <h1>Les avantages des applications pointeur laser</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
-
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
-
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
-    </a>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+<p>Le magasin de pointeurs laser (<strong><a href="https://www.laserpointeur.com/">www.laserpointeur.com</a></strong>) est devenu un service de plus en plus populaire pour les applications de l'industrie aérospatiale qui nécessitent un assemblage rapide et une précision de qualité. Cependant, grâce à l'introduction de nouveaux matériaux, technologies et applications au fil des ans, le pointeur laser a non seulement pris de l'ampleur, mais s'est avéré être un service extrêmement précieux dans les applications aérospatiales, à la fois en minimisant les zones affectées par la chaleur et en prévenant les dommages aux composants emballés.</p>
+<p>La première chose à regarder est de savoir qui va acheter un pointeur laser ? Y aura-t-il un entrepreneur en électricité professionnel impliqué dans le projet ? Sinon, vous voudrez peut-être opter pour une option basse tension. Le pointeur laser puissant peut provoquer un choc électrique, mais pas de blessure grave. D'un autre côté, l'alimentation de 120 volts est dangereuse et peut infliger de graves dommages et même la mort aux animaux humains.</p>
+<p><em>Si vous travaillez avec une nouvelle construction, vous pouvez parfois planifier cette tranchée pour coïncider avec d'autres besoins tels que l'irrigation. Cela réduira vos coûts et vous fera gagner du temps pour ces puissants pointeurs laser. Vous ferez l'expérience de la chute de tension minimale du puissant pointeur laser. Vous pourrez utiliser des pointeurs laser très puissants.</em></p>
+<p>Une grande variété de pointeurs laser est disponible. Aucun conduit n'est requis pour les câbles, ce qui permet d'économiser de la main-d'œuvre et des coûts de matériel. Ces dispositifs laser fournissent des sources d'énergie alternatives et se présentent sous une grande variété de formes, de tailles et de conceptions, s'avérant être un outil vital dans le secteur aérospatial.</p>
+<h3>Les avantages du module laser</h3>
+<p>Le module laser est un service compétent qui joue un rôle dans la fourniture d'une qualité de processus fiable et reproductible pour les matériaux aérospatiaux soudés. Les trois principaux avantages du pointeur laser incluent :</p>
+<p>Ces pointeurs laser aident à prévenir l'oxydation des matériaux (c'est-à-dire l'aluminium et le titane) en créant une atmosphère exempte d'oxygène et d'humidité pour produire en toute sécurité des pièces aérospatiales.</p>
+<p><a href="https://www.laserpointeur.com/c-40_81.html"><img alt="Laser Module" src="https://www.laserpointeur.com/images/p/m/160321/936.jpg"></a></p>
+<p><strong>Aucun défaut - Ce module laser bleu élimine les fissures dues à la contamination atmosphérique, ce qui conduit à la création de modules laser de haute qualité.</strong></p>
+<p>Rentabilité élevée - Les pièces peuvent être transférées efficacement vers la chambre principale sans que les fabricants aient besoin de faire fonctionner manuellement des systèmes de gaz de protection coûteux. Nous pouvons prendre en charge vos applications aérospatiales avec des capacités de traitement laser internes complètes.</p>
 @endsection
