28a2dba1f62d917a4fce461f49941fe14f8a2aa9
vigossjame <gabinvigoss@gmail.com>
2022-04-18 00:19:46 +0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,16 +1,15 @@
 @extends('_layouts.master')
 
-@section('title', 'About')
+@section('title', 'Les avantages du laser')
 
 @section('content')
-    <h1>About</h1>
+<h1>Les avantages du laser</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
-
-    <h2>Links:</h2>
-
-    <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
-    </ul>
+    <p>L'une des idées fausses les plus courantes sur la technologie laser est que les pointeurs laser sont limités en volume. Le fait est que les progrès de la technologie de traitement au laser ont rendu les procédés au laser tels que le pointeur laser très rapides et que la plupart des fabricants qui en sont équipés utilisent les outils pour une production à grande échelle.</p>
+    <p>Une autre idée fausse courante est que les pointeurs laser sont un danger inutile sur le lieu de travail. Les fabricants de lasers conçoivent leurs systèmes à partir de zéro avec la sécurité comme principale préoccupation. Le pointeur laser est disponible en deux tailles. Il y a une taille adulte et aussi une taille parfaite pour les jeunes. En raison du volume considérable, il est impératif de continuer à travailler de manière ordonnée et sûre.</p>
+    <p>Les lasers sont utilisés avec succès pour la découpe de matériaux et d'autres processus industriels depuis plus de 40 ans et ont fait leurs preuves au cours de cette période. En plus d'être à la fois efficaces et sûrs, ils peuvent également être plus simples à utiliser - ils ne nécessitent pas les outils et matrices complexes que les méthodes traditionnelles font et parce qu'ils sont sans contact, il n'y a pas d'usure d'outil pour affecter la qualité de coupe.</p>
+    <p><strong>Ils sont sûrs, efficaces, rapides et évolutifs jusqu'à la production à grande échelle - mais ces avantages ne font qu'effleurer la surface de ce que le pointeur laser peut offrir à votre prochain projet.</strong></p>
+    <p>Exclusivement contrôlés par CNC, les systèmes de pointeur laser peuvent créer le faisceau avec une précision inégalée, y compris des largeurs très étroites, et maintenir les tolérances les plus strictes. La configuration est rapide et le faisceau est fiable et facilement reproductible. Les systèmes de pointeur laser sont également capables de découper des formes avec des géométries très complexes, au-delà des capacités des autres méthodes d'usinage.</p>
+    <p><em>Malgré la puissance qu'ils créent - les lasers industriels fonctionnent généralement dans la plage de 10 à 3000 W - les systèmes de pointeur laser produisent des niveaux de chaleur relativement faibles, ce qui minimise la déformation du matériau et nécessite moins d'énergie d'entrée que les machines standard. Vous pouvez même trouver des pointeurs laser pour vos animaux de compagnie.</em></p>
+    <p>Sans contact entre la pièce et l'appareil de coupe, les pointeurs laser éliminent le risque de contamination du matériau. Lorsque l'on considère que le pointeur laser peut être utilisé pour une grande variété de matériaux - y compris une gamme de plastiques et de métaux, de caoutchoucs, de bois, de céramique, etc. - cela devient très important. Ils viennent en rouge, ambre, vert, bleu et blanc. Ils sont résistants aux chocs et peuvent même être écrasés par un véhicule tout en continuant à fonctionner.</p>
 @endsection
